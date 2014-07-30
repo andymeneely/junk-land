7ba0ad24e05c1d4a0128c24118380fa7d41fec50
Andy Meneely <andy@se.rit.edu>
2014-07-29 23:47:10 -0400

    Starting to massage the data in

--- deck.rb
+++ deck.rb
@@ -1,6 +1,22 @@
 require 'squib'
+require 'pp'
 
-Squib::Deck.new(cards: 3) do 
-  text str: "Hello, World!"
+deck = Squib.xlsx file: 'junk.xlsx'
+junk = deck['Type'].select {|t| %w{item blueprint victory}.include? t }
+img = 'color'
+
+#TODO put this into excel
+bgimage = {
+  'item' => "#{img}/item-background.png",
+  'blueprint' => "#{img}/blueprint-background.png"
+}
+
+# Items and Blueprints decks
+Squib::Deck.new(cards: junk.size) do 
+  
+
+  png file: junk.collect {|j| bgimage[j] }
+
+  Squib::logger.info 'Saving...'
   save format: :png
 end
