e8985ac5c7ddca18b1a501c64e586ffa3d9e47af
Andy Meneely <andy@se.rit.edu>
2014-08-13 13:39:23 -0400

    Centering if there's just one bonus

--- deck.rb
+++ deck.rb
@@ -9,6 +9,10 @@ img = 'color'
 # Replace spaces with programmer-friendly underscores
 %w(Bonus1Type Bonus2Type).each { |b| underscorify(deck[b]) }
 
+#Layout Bonus1 to the left if there's two bonuses, middle otherwise
+bonus1_layout = deck['Bonus2Type'].collect {|t| t.to_s.empty? ? :bonus1_middle : :bonus1_left }
+bonus1_layout_text = bonus1_layout.collect {|l| "#{l}_text"}
+
 bgimage = {
   'item' => "#{img}/item-background.png",
   'blueprint' => "#{img}/blueprint-background.png",
@@ -31,15 +35,14 @@ Squib::Deck.new(cards: junk.size, config: 'config.yml', layout: 'junk.yml') do
     text range: range, str: deck[resource], layout: "#{resource}_text"
   end
 
-  svg file: 'junk-bonuses.svg', id: deck['Bonus1Type'], layout: :bonus1, force_id: true
-  text str: deck['Bonus1Num'], layout: :bonus1_text, color: :black
+  svg file: 'junk-bonuses.svg', id: deck['Bonus1Type'], layout: bonus1_layout, force_id: true
+  text str: deck['Bonus1Num'], layout: bonus1_layout_text, color: :black
 
   svg file: 'junk-bonuses.svg', id: deck['Bonus2Type'], layout: :bonus2, force_id: true
   text str: deck['Bonus2Num'], layout: :bonus2_text, color: :black
 
   png file: 'tgc-proof-overlay.png', alpha: 0.5
-  save range: id['Saggy Park Bench'], 
-      format: :png
+  #save range: id['Sack of Door Knobs']..id['Rusty Bicycle Chain'], format: :png
   
-  # save format: :png
+  save format: :png
 end
